514a9d5b12564e200e224abcae7e14b4c8d3abef
anne-other <you@example.com>
2019-07-27 21:12:28 +0100

    Adding method names to buttons

--- app/src/main/java/com/anneother/whatsfordinner/MainActivity.java
+++ app/src/main/java/com/anneother/whatsfordinner/MainActivity.java
@@ -3,6 +3,7 @@ package com.anneother.whatsfordinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onGoToIngredients(View view) {
+
+    }
+
+    public void onGoToRecipes(View view) {
+
     }
 }
